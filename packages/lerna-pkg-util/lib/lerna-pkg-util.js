e41f1995f13d74e242c8cfccde001c30ef249cd7
mind <mind.wang@lixinchuxing.com>
2020-10-19 21:23:35 +0800

    fix：0.0.3

--- packages/lerna-pkg-util/lib/lerna-pkg-util.js
+++ packages/lerna-pkg-util/lib/lerna-pkg-util.js
@@ -1,14 +1,16 @@
 'use strict';
 
-const dayjs = require('dayjs')
+const dayjs = require('dayjs');
 
 module.exports = lernaPkgUtil;
 
 function lernaPkgUtil() {
-    console.log('lernaPkgUtil')
+    console.log('1lernaPkgUtil2');
     // TODO
 
-    const formatDate = dayjs().format('YYYY-MM-DD')
-    
-    return formatDate
+    const formatDate = dayjs().format('YYYY-MM-DD');
+
+    console.log('formatDate', formatDate);
+
+    return formatDate;
 }
